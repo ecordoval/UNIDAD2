7555177a96c7b6d26651ecb4e18df2046eef5675
ecordoval <ecordoval@iesch.org>
2025-10-17 19:55:39 +0200

    cambios 17/10/2025

--- src/UD2_Eje5_2.java
+++ src/UD2_Eje5_2.java
@@ -13,19 +13,33 @@ public class UD2_Eje5_2 {
         double ramdom1 =Math.random();
         double ramdom2 =Math.random();
         double ramdom3 =Math.random();
+        int numeroAleatorio1 = 0;
+        int numeroAleatorio2 = 0;
+        int numeroAleatorio3 = 0;
 
-        if(numero >=0 && numero<=10)
+        if(numero >=0 && numero<=1)
         {
-
-            int numeroAleatorio1 = (int) (ramdom1 * 10)-numero;
-            System.out.println("Los números aleatorios son: "+Math.abs(numeroAleatorio1));
-
-        }
-        else{
-
-            //
+            numeroAleatorio1 = (int) (ramdom1);
+            numeroAleatorio3 = (int) (ramdom2);
+            numeroAleatorio2 = (int) (ramdom3);
 
         }
+
+        if(numero >=2 && numero<=10)
+        {
+            numeroAleatorio1 = (int) (ramdom1 * 10)-numero;
+            numeroAleatorio2 = (int) (ramdom2 * 10)-numero;
+            numeroAleatorio3 = (int) (ramdom3 * 10)-numero;
+
+        }
+        if(numero >=11 && numero<=100)
+        {
+            numeroAleatorio1 = (int) (ramdom1 * 100)-numero;
+            numeroAleatorio2 = (int) (ramdom2 * 100)-numero;
+            numeroAleatorio3 = (int) (ramdom3 * 100)-numero;
+
+        }
+
 //        double ramdom1 =Math.random();
 //        int numeroAleatorio1 = (int) (ramdom1 * 100)-numero;
 //
@@ -36,6 +50,7 @@ public class UD2_Eje5_2 {
 //        int numeroAleatorio3 = (int) (ramdom3 * 100)-numero;
 
         System.out.println(ramdom1+ " "+ ramdom2 + " " + ramdom3);
+        System.out.println("Los números aleatorios son: "+Math.abs(numeroAleatorio1)+" " + Math.abs(numeroAleatorio2)+" " + Math.abs(numeroAleatorio3));
 
    //    System.out.println("Los números aleatorios son: "+Math.abs(numeroAleatorio1));//+ " " + Math.abs(numeroAleatorio2)+" " + Math.abs(numeroAleatorio3));
 
