351789c930e2e5f576b76b6f7aed822ff5b7fcf9
ecordoval <ecordoval@iesch.org>
2025-10-17 20:35:44 +0200

    cambios 17/10/2025

--- src/UD2_Eje5_2.java
+++ src/UD2_Eje5_2.java
@@ -13,46 +13,12 @@ public class UD2_Eje5_2 {
         double ramdom1 =Math.random();
         double ramdom2 =Math.random();
         double ramdom3 =Math.random();
-        int numeroAleatorio1 = 0;
-        int numeroAleatorio2 = 0;
-        int numeroAleatorio3 = 0;
 
-        if(numero >=0 && numero<=1)
-        {
-            numeroAleatorio1 = (int) (ramdom1);
-            numeroAleatorio3 = (int) (ramdom2);
-            numeroAleatorio2 = (int) (ramdom3);
+        int numeroAleatorio1 = (int) (ramdom1 * numero)+ 1;
+        int numeroAleatorio2 = (int) (ramdom2 * numero)+ 1;
+        int numeroAleatorio3 = (int) (ramdom3 * numero)+ 1;
 
-        }
-
-        if(numero >=2 && numero<=10)
-        {
-            numeroAleatorio1 = (int) (ramdom1 * 10)-numero;
-            numeroAleatorio2 = (int) (ramdom2 * 10)-numero;
-            numeroAleatorio3 = (int) (ramdom3 * 10)-numero;
-
-        }
-        if(numero >=11 && numero<=100)
-        {
-            numeroAleatorio1 = (int) (ramdom1 * 100)-numero;
-            numeroAleatorio2 = (int) (ramdom2 * 100)-numero;
-            numeroAleatorio3 = (int) (ramdom3 * 100)-numero;
-
-        }
-
-//        double ramdom1 =Math.random();
-//        int numeroAleatorio1 = (int) (ramdom1 * 100)-numero;
-//
-
-//        int numeroAleatorio2 = (int) (ramdom2 * 100)-numero;
-//
-
-//        int numeroAleatorio3 = (int) (ramdom3 * 100)-numero;
-
-        System.out.println(ramdom1+ " "+ ramdom2 + " " + ramdom3);
-        System.out.println("Los números aleatorios son: "+Math.abs(numeroAleatorio1)+" " + Math.abs(numeroAleatorio2)+" " + Math.abs(numeroAleatorio3));
-
-   //    System.out.println("Los números aleatorios son: "+Math.abs(numeroAleatorio1));//+ " " + Math.abs(numeroAleatorio2)+" " + Math.abs(numeroAleatorio3));
+        System.out.println("Los números aleatorios son: "+numeroAleatorio1+" " + numeroAleatorio2+" " + numeroAleatorio3);
 
 
 
