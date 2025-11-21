69d237bcbba17624cec896fb5676a6b31a417414
ecordoval <ecordoval@iesch.org>
2025-11-21 11:58:30 +0100

    21/11/2025

--- src/UD4_Eje7.java
+++ src/UD4_Eje7.java
@@ -5,13 +5,30 @@ public class UD4_Eje7 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("Ingrese su nota (0-10) :");
-        while  (!sc.hasNextDouble() && sc.nextDouble() <=10 && sc.nextDouble() >0)
+
+
+        do{
+            System.out.print("Ingrese su nota (0-10) :");
+
+        }
+        while(!sc.hasNextDouble())
+
+
+        while  (!sc.hasNextDouble() || ( (sc.nextDouble() < 0 || sc.nextDouble() > 10)))
         {
             sc.nextLine();
             System.out.print("Ingrese su nota (0-10) :");
         }
+
         double nota = sc.nextDouble();
+//        while  (nota < 0 || nota > 10)
+//        {
+//            sc.nextLine();
+//            System.out.print("Ingrese su nota (0-10) :");
+//            nota = sc.nextDouble();
+//        }
+
+        System.out.println(nota);
 
         System.out.println("Su nota es: "+ convertirATexto(nota));
 
