5e2aea9dbaca8d3921fdd80082095c085c70fee0
ecordoval <ecordoval@iesch.org>
2025-11-21 17:07:01 +0100

    cambios 21/11/2025

--- src/UD4_Eje14.java
+++ src/UD4_Eje14.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 
 public class UD4_Eje14 {
+
+    static int suma =0;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Ingrese un número entero:");
@@ -10,48 +12,35 @@ public class UD4_Eje14 {
             System.out.print("Ingrese un número entero:");
         }
 
-        int numero =  sc.nextInt();
+        int numero = sc.nextInt();
 
-        System.out.println("La suma de las cifras es: "+sumaCifras(numero));
+        System.out.println("La suma de las cifras es: " + sumaCifras(numero));
 
         sc.close();
 
     }
 
-    private static int sumaCifras(int numero)
-    {
-        if (numero == 1)
-        {
-            System.out.print(0);
-        }
-        else {
-            int a = 0, b = 1;
-            System.out.print(a + ", " + b);
-            for (int i = 0; i <= numero - 3; i++) {
-                int siguiente = a + b;
-                System.out.print(", " + siguiente);
-                a = b;
-                b = siguiente;
-            }
-        }
+    private static int sumaCifras(int numero) {
 
+        if (numero == 0) {
+            return suma;
+        } else {
+            int cifra= numero % 10;
+            suma = suma +cifra;
+            return sumaCifras(numero/10);
+
+        }
 
     }
 }
-
-
-564
-
+        /*
         564%10 = 4
         564/10 = 56
 
         56%10 = 6
-        56/10 = 50
+        56/10 = 5
 
-        50%10 = 5
-        50/10 = 5
+        5%10 = 5
+        5/10 = 0
 
-
-
-
-
+*/
